ade412f7f07ec33f33f70446f1b4c90fb5b5e096
micheleq17 <michelequaglia6@gmail.com>
2024-06-01 13:03:09 +0200

    ID-1 RESOLVED

--- src/it/unisa/control/HomeServlet.java
+++ src/it/unisa/control/HomeServlet.java
@@ -3,6 +3,8 @@ package it.unisa.control;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,28 +16,36 @@ import javax.servlet.http.HttpServletResponse;
 import it.unisa.model.ProdottoBean;
 import it.unisa.model.ProdottoDao;
 
-/**
- * Servlet implementation class HomeServlet
- */
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
- 
+
+	private static final List<String> VALID_PAGES = Arrays.asList(
+		"Home.jsp",
+		"Ps5.jsp",
+		"XboxSeries.jsp",
+		"Switch.jsp",
+		"Ps4.jsp",
+		"XboxOne.jsp"
+	);
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		ProdottoDao dao = new ProdottoDao();
-		
 		ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
 		String redirectedPage = request.getParameter("page");
-		
+
+		// Verifica se il parametro page è valido
+		if (redirectedPage == null || !VALID_PAGES.contains(redirectedPage)) {
+			redirectedPage = "Home.jsp"; // Imposta la pagina predefinita
+		}
+
 		try {
 			ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
 			ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
 			ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
 			ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
 			ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
-			
+
 			categorie.add(PS5);
 			categorie.add(XboxSeries);
 			categorie.add(Switch);
@@ -43,21 +53,16 @@ public class HomeServlet extends HttpServlet {
 			categorie.add(Xone);
 
 			request.getSession().setAttribute("categorie", categorie);
-			
 
-			
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
 		dispatcher.forward(request, response);
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		doGet(request, response);
 	}
-
 }
